7eb4fd80069d4e4db223f0e3d8607141425de0d2
Bezrukov Andrey <andrbezr2016@yandex.ru>
2024-12-19 10:42:04 +0300

    update implementation

--- src/main/java/com/andrbezr2016/backend/client/config/ClientProperties.java
+++ src/main/java/com/andrbezr2016/backend/client/config/ClientProperties.java
@@ -5,7 +5,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.bind.ConstructorBinding;
 
 @Getter
-@ConfigurationProperties("backend-client")
+@ConfigurationProperties("backend-client-service.client")
 public class ClientProperties {
 
     @ConstructorBinding
